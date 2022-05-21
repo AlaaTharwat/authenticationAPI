6244d031c80cf2e724e77ff6c05d72da166cd7b9
alaaTharwat <alaa.tharrwat@gmail.com>
2022-05-21 11:29:56 +0200

    change connection to localhost

--- startup/dbConnection.js
+++ startup/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const winston = require("winston");
 module.exports = function () {
   mongoose
-    // mongodb://localhost:27017/authapp => to run without docker containers
+    // mongodb://mongo:27017/authapp => to run with docker 
     .connect("mongodb://localhost:27017/authapp")
     .then(() => winston.info("connected to db "), console.log("Connection to database!"))
     .catch(e => console.log(e));
